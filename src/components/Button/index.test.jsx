9345e808bd3ebed7b8cbbf7a347eddbc64320a29
Joanna Erd <joanna.erd@gmail.com>
2020-09-24 22:31:41 +0200

    Add unit tests for Button css classes

--- src/components/Button/index.test.jsx
+++ src/components/Button/index.test.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-import { Button } from '.';
+import { Button, BUTTON_VARIANTS } from '.';
 
 it('renders the content', () => {
   const { getByText } = render(<Button>Hi!</Button>);
@@ -20,4 +20,39 @@ it('calls onClick when clicked', () => {
     userEvent.click(button);
 
     expect(onClick).toHaveBeenCalled();
+});
+
+it('has correct classes by default', () => {
+    const { getByText } = render(<Button>Good morning.</Button>);
+  
+    const button = getByText(/good morning/i);
+
+    expect(button.classList).toContain('Button');
+});
+
+it('has correct classes in "primary" variant', () => {
+    const { getByText } = render(<Button variant={BUTTON_VARIANTS.primary}>Good afternoon.</Button>);
+  
+    const button = getByText(/good afternoon/i);
+
+    expect(button.classList).toContain('Button');
+    expect(button.classList).toContain('Button--primary');
+});
+
+it('has correct classes in "secondary" variant', () => {
+    const { getByText } = render(<Button variant={BUTTON_VARIANTS.secondary}>Good evening.</Button>);
+  
+    const button = getByText(/good evening/i);
+
+    expect(button.classList).toContain('Button');
+    expect(button.classList).toContain('Button--secondary');
+});
+
+it('does not add non-existant modifiers', () => {
+    const { getByText } = render(<Button variant="canine">Woof.</Button>);
+  
+    const button = getByText(/woof/i);
+
+    expect(button.classList).toContain('Button');
+    expect(button.classList).not.toContain('Button--canine');
 });
